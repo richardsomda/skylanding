a7cbfe653f236a959877cafe3cd133a4684ff5b3
Joel Senyo <116897993+Senyoj@users.noreply.github.com>
2023-11-20 13:39:30 -1000

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,7 +6,7 @@ function Contact() {
     <div className="py-2">
        <button className="font-semibold bg-white  w-auto   p-3 rounded-xl text-herotext md:text-3xl 
             md:p-5 md:rounded-2xl lg:p-5 lg:text-2xl lg:w-[10rem]">
-              Let's Talk
+              Let&apos;s Talk
             </button>
     </div>
   );
